b7843198c00cf85d13f85f14abe459038f225d1e
Zabi <mzabiu@gmail.com>
2021-03-29 16:08:30 +0530

    Modified the code to generate the new log file everytime for execution

--- src/test/java/com/orangehrm/framework/components/MyLog.java
+++ src/test/java/com/orangehrm/framework/components/MyLog.java
@@ -1,17 +1,23 @@
 package com.orangehrm.framework.components;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
 public class MyLog implements Constants {
 
-	public static Logger log = Logger.getLogger(MyLog.class.getName());
-
 	private static final String propertyPath = CURRENT_DIR + ReadPropertiesFile.getProperties().get(LOGJ_PATH);
 
 	static {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy-hh-mm-ss");
+		System.setProperty("current.date.time", dateFormat.format(new Date()));
 		PropertyConfigurator.configure(propertyPath);
+
 	}
+
+	public static Logger log = Logger.getLogger(MyLog.class.getName());
 
 	public static void logInfo(String message) {
 		log.info(message);
