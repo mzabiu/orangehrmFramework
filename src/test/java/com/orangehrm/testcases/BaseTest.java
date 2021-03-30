c998b6d212171c62f07e6952f37499c6b7d86054
Zabi <mzabiu@gmail.com>
2021-03-30 19:35:00 +0530

    Code updated

--- src/test/java/com/orangehrm/testcases/BaseTest.java
+++ src/test/java/com/orangehrm/testcases/BaseTest.java
@@ -51,17 +51,9 @@ public class BaseTest implements Constants {
 		String browserName = xmlData.getParameter("browser").toLowerCase();
 
 		if (browserName.equalsIgnoreCase("chrome")) {
-			WebDriverManager.chromedriver().setup();
-			ChromeOptions chromeOptions = new ChromeOptions();
-
-			MyLog.logInfo("Path of chrome is " + APP_DATA + configPropertyData.get(CHROME_BINARY_PATH));
-			if (System.getProperty("jenkins.buildurl") != null) {
-				MyLog.logInfo("The execution is triggered from jenkins and the url is "
-						+ System.getProperty("jenkins.buildurl"));
-
-				chromeOptions.setBinary(configPropertyData.get(CHROME_BINARY_PATH));
-			}
-			driver1 = new ChromeDriver(chromeOptions);
+			System.setProperty("webdriver.chrome.driver",
+					CURRENT_DIR + configPropertyData.get(DRIVERS_PATH) + "chromedriver.exe");
+			driver1 = new ChromeDriver();
 		} else if ((browserName.equalsIgnoreCase("edge"))) {
 			System.setProperty("webdriver.edge.driver", "/src/main/resources/com/orangehrm/drivers/msedgedriver.exe");
 			driver1 = new EdgeDriver();
